3b29299a9513e46356ea1a45778aa2da1f3b7d29
Edney <edney_reis999@hotmail.com>
2020-03-09 12:26:08 -0300

    spring initializr

---
+++ src/main/java/br/com/edney/project/alfa/TemplateGraphQL/TemplateGraphQlApplication.java
@@ -0,0 +1,13 @@
+package br.com.edney.project.alfa.TemplateGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateGraphQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edney/project/alfa/TemplateGraphQL/TemplateGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edney.project.alfa.TemplateGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
